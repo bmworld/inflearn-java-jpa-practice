cc1b98394160637ec6dfc40469da10916b9f8cee
sbkim <victory289@naver.com>
2023-04-16 16:36:42 +0900

    JPA WAS 개발 - 상품 도메인

--- 02-webApp/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ 02-webApp/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
 import jpabook.jpashop.domain.order.OrderItem;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,24 +11,47 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Getter @Setter
+@Getter
+@Setter
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype")
 public abstract class Item {
-    @Id
-    @GeneratedValue
-    @Column(name = "item_id")
-    private Long id;
+  @Id
+  @GeneratedValue
+  @Column(name = "item_id")
+  private Long id;
 
-    private String name;
+  private String name;
 
-    private int price;
+  private int price;
 
-    private int stockQuantity;
+  private int stockQuantity;
 
-    @ManyToMany(mappedBy = "items")
-    private List<Category> categories = new ArrayList<>();
+  @ManyToMany(mappedBy = "items")
+  private List<Category> categories = new ArrayList<>();
 
-    @OneToMany(mappedBy = "item")
-    private List<OrderItem> orderItems = new ArrayList<>();
+  @OneToMany(mappedBy = "item")
+  private List<OrderItem> orderItems = new ArrayList<>();
+
+
+  // ** 비즈니스 로직 **
+  /**
+   * 재고 증가
+   */
+  public void addStock(int quantity) {
+    this.stockQuantity += quantity;
+  }
+
+  /**
+   *  재고 감소
+   */
+  public void removeStock(int quantity) {
+    int restStock = this.stockQuantity - quantity;
+
+    if (restStock < 0) {
+      throw new NotEnoughStockException("Need more stock");
+    }
+    this.stockQuantity -= quantity;
+
+  }
 }
