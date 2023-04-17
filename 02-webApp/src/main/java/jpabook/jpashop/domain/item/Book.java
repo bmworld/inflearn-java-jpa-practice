8efb312edd896de9cf83fa220e5848c60f700570
sbkim <victory289@naver.com>
2023-04-17 20:23:23 +0900

    JPA WAS 개발 - 웹 계층 개발 (1st)
     - 홈화면
     - 레이아웃
     - 회원 등록
     - 회원 목록 조회
     - 회원 목록 조회
     - 상품 등록
     - 상품 목록
     - 상품 수정

--- 02-webApp/src/main/java/jpabook/jpashop/domain/item/Book.java
+++ 02-webApp/src/main/java/jpabook/jpashop/domain/item/Book.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 
+import jpabook.jpashop.controller.BookForm;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,7 +14,24 @@ import javax.persistence.Entity;
 @Setter
 public class Book extends Item {
 
-    private String artist;
-    private String etc;
+    private String author;
+    private String isbn;
+
+    public void createItem(BookForm form) {
+        super.setName(form.getName());
+        super.setPrice(form.getPrice());
+        super.setStockQuantity(form.getStockQuantity());
+        this.author = form.getAuthor();
+        this.isbn = form.getIsbn();
+    }
+
+    public void updateItem(BookForm form) {
+        super.setId(form.getId());
+        super.setName(form.getName());
+        super.setPrice(form.getPrice());
+        super.setStockQuantity(form.getStockQuantity());
+        this.author = form.getAuthor();
+        this.isbn = form.getIsbn();
+    }
 
 }
