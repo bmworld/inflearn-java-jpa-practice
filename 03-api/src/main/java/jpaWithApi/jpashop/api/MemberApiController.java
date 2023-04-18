1134063f6be05f81cda73266c35d8b527c1707c3
sbkim <victory289@naver.com>
2023-04-18 23:49:23 +0900

    JPA with API - API 개발 기본 (* API Spec 정의 시, Entity가 아닌 RequestDTO를 사용하시라)

--- 03-api/src/main/java/jpaWithApi/jpashop/api/MemberApiController.java
+++ 03-api/src/main/java/jpaWithApi/jpashop/api/MemberApiController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,18 +20,39 @@ public class MemberApiController {
 
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+        // Entity를 parameter 받지도 말고, 외부에 노출하지도 마시라 => 왜냐면 Entity는 변경될 확률이 높다. => Entity가 변경되는 순간, 관련 API 에러터짐
+        // 그러니, API 마다 각각 RequestDTO 만드시라
         Long memberId = memberService.join(member);
         return new CreateMemberResponse(memberId);
-
     }
+
+    @PostMapping("/api/v2/members")
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
+        // 꼭 Request DTO를 사용하시라
+        // Entity와 Presentation Layer를 분리할 수 있다!
+        // Entity가 변경되어도 API Spec이 변하지 않아야 한다.
+        Member member = new Member();
+        member.setName(request.getName());
+        Long memberId = memberService.join(member);
+        return new CreateMemberResponse(memberId);
+    }
+
+
+
+    @Data
+    static class CreateMemberRequest {
+        @NotEmpty
+        private String name;
+    }
+
 
     @Data
     static class CreateMemberResponse {
-        private Long id;
 
+        private Long id;
         public CreateMemberResponse(Long id) {
             this.id = id;
         }
-    }
 
+    }
 }
--- 03-api/src/main/java/jpaWithApi/jpashop/domain/member/Member.java
+++ 03-api/src/main/java/jpaWithApi/jpashop/domain/member/Member.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,6 +21,7 @@ public class Member {
     private Long id;
 
 
+    @NotEmpty
     private String name;
 
     @Embedded
