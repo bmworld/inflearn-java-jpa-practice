d647e4cd77e8cef484004f1e13db24b44ffd87b3
sbkim <victory289@naver.com>
2023-04-24 21:49:54 +0900

    Spring Data JPA - 도메인 Model 및 동작 확인

--- 04-data-jpa/src/main/java/study/datajpa/domain/Member.java
+++ 04-data-jpa/src/main/java/study/datajpa/domain/Member.java
@@ -1,27 +1,39 @@
 package study.datajpa.domain;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.*;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)// JPA 표준 스펙 중, Entity는 기본 생성자 필요함. => Why? JPA Proxy 사용 시, private 접근자일 경우 불가하므로, protected로 설정한다.
+@ToString(of = {"id", "username", "age"}) // Entity 연관관계 필드는 무한 루프 방지를 위해, 제외하시라.
 public class Member {
     @Id @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
-
     private String name;
+    private int age;
 
-    protected Member() {
-        // JPA 표준 스펙 중, Entity는 기본 생성자 필요함.
-        // JPA Proxy 사용 시, private 접근자일 경우 불가하므로, protected로 설정한다.
-    }
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
 
     public Member(String name) {
         this.name = name;
+    }
+
+    public Member(String name, int age, Team team) {
+        this.name = name;
+        this.age = age;
+        changeTeam(team);
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
     }
 }
